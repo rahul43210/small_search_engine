631e01d3c1e73e9c150d6f75f110709be2cf97e6
Rahul Kumar <rahul.kumar.43210@gmail.com>
2026-05-13 17:28:06 +0530

    first commit

---
+++ src/main/java/com/searchengine/ssearchapp/SsearchappApplication.java
@@ -0,0 +1,13 @@
+package com.searchengine.ssearchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsearchappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsearchappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/searchengine/ssearchapp/SsearchappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchengine.ssearchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsearchappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
